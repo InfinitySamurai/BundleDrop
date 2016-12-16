82033ef00a7cd7b39eae1e03a5232fb33d980cb8
Russell Long <russell._long@hotmail.com>
2016-12-16 13:22:15 +1100

    Updated program to enable code generation

--- BundleDrop/Program.cs
+++ BundleDrop/Program.cs
@@ -5,16 +5,22 @@ namespace BundleDrop {
     /// <summary>
     /// The main class.
     /// </summary>
-    public static class Program
-    {
+    public static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
-        {
-            using (var game = new Game1())
-                game.Run();
+        static void Main(string[] args) {
+            if (args.Length > 0) {
+                if (args[0] == "generate") {
+                    Console.WriteLine("Program start, generating files");
+                    CodeGenerator.Generate();
+                }
+            }
+            else {
+                using (var game = new Game())
+                    game.Run();
+            }
         }
     }
 #endif
